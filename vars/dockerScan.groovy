68234abc804654ec102f93397e0c0a4ca64c7a67
Kobidav <Kobidav@users.noreply.github.com>
2025-06-28 13:09:01 +0300

    Update dockerScan.groovy

--- vars/dockerScan.groovy
+++ vars/dockerScan.groovy
@@ -3,10 +3,6 @@ def call(String imageName) {
     
     // Example using Trivy
     sh """
-        if ! command -v trivy &> /dev/null; then
-            echo 'Installing Trivy...'
-            curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sh -s -- -b /usr/local/bin
-        fi
         trivy image --exit-code 1 --severity CRITICAL,HIGH ${imageName}
     """
 }
